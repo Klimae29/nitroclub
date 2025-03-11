6f05507222044d55ca55b9799c598ba83b673f83
Mikael Renvoise <klimae@icloud.com>
2025-03-11 12:25:07 +0100

    correction 4

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,5 +4,5 @@ class Car < ApplicationRecord
   validates :style, presence: true, inclusion: { in: ["stance", "muscle", "neon", "sono", "offroad", "mini"] }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :description, presence: true, lenght: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
