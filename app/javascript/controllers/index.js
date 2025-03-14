addb25837df4a51b05fee70c0aaec4d434127495
Mikael Renvoise <klimae@icloud.com>
2025-03-14 15:26:03 +0100

    categories controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 import { Application } from "@hotwired/stimulus";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
-import CategoriesController from "./categories_controller";
+//import CategoriesController from "./categories_controller";
 
 const application = Application.start();
 eagerLoadControllersFrom("controllers", application);
